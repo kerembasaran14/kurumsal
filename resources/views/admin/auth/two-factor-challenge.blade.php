bd3c918625e13a50ceed5c3fec91c0fbd741a37d
kerembasaran <kerembasaran14@gmail.com>
2022-12-06 00:33:11 +0300

    Laravel Fortify paketi iki faktörlü kimlik doğrulama kurtarma kodları giriş sayfası eklendi

--- resources/views/admin/auth/two-factor-challenge.blade.php
+++ resources/views/admin/auth/two-factor-challenge.blade.php
@@ -36,6 +36,29 @@
                     <div class="card-footer text-center">
                     </div>
                 </div>
+                <div class="card shadow-lg border-0 rounded-lg mt-5 mb-5">
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">İki Faktörlü Kurtarma Kodu</h3></div>
+                    <div class="card-body">
+                        Lütfen oturum açmak için kimlik doğrulama kodunuzu girin.
+                        <form method="POST" action="{{ route('two-factor.login') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label class="small mb-1" for="recovery_code">Kurtarma Kodu</label>
+                                <input class="form-control py-4 @error('recovery_code') is-invalid @enderror" id="recovery_code" type="password" placeholder="İki Faktörlü Kurtarma Kodunuzu Giriniz" name="recovery_code" required>
+                                @error('recovery_code')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                <button class="btn btn-primary" type="submit">Gönder</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-center">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
